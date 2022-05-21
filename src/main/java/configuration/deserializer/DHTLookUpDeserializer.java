e38479e3d8f9b7901935abb2e41d6680fb154af5
sepgh <sepehrgh01@gmail.com>
2022-05-21 12:29:08 +0430

    feat: DHTLookUpDeserializer

---
+++ src/main/java/configuration/deserializer/DHTLookUpDeserializer.java
@@ -0,0 +1,25 @@
+package configuration.deserializer;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import common.NettyBigIntegerExternalNode;
+import common.NettyConnectionInfo;
+import io.ep2p.kademlia.protocol.message.DHTLookupKademliaMessage;
+
+import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.math.BigInteger;
+
+public class DHTLookUpDeserializer<K extends Serializable> implements JsonDeserializer<DHTLookupKademliaMessage.DHTLookup<BigInteger, NettyConnectionInfo, K>> {
+
+    @Override
+    public DHTLookupKademliaMessage.DHTLookup<BigInteger, NettyConnectionInfo, K> deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        DHTLookupKademliaMessage.DHTLookup<BigInteger, NettyConnectionInfo, K> dhtLookup = new DHTLookupKademliaMessage.DHTLookup<>();
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        JsonObject requesterJsonObject = jsonObject.getAsJsonObject("requester");
+        dhtLookup.setRequester(jsonDeserializationContext.deserialize(requesterJsonObject, NettyBigIntegerExternalNode.class));
+        dhtLookup.setCurrentTry(jsonObject.get("currentTry").getAsInt());
+        dhtLookup.setKey(jsonDeserializationContext.deserialize(jsonObject.getAsJsonObject("key"), new TypeToken<K>() {}.getType()));
+        return dhtLookup;
+    }
+}
