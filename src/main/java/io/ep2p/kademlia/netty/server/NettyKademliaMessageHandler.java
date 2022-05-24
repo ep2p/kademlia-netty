ae7584d2757e287bf3c8821f9152cf022c62527f
sepgh <sepehrgh01@gmail.com>
2022-05-24 19:16:55 +0430

    feat: implement NettyKademliaMessageHandler.NettyKademliaMessageHandler()

--- src/main/java/io/ep2p/kademlia/netty/server/NettyKademliaMessageHandler.java
+++ src/main/java/io/ep2p/kademlia/netty/server/NettyKademliaMessageHandler.java
@@ -1,6 +1,8 @@
 package io.ep2p.kademlia.netty.server;
 
+import com.google.gson.reflect.TypeToken;
 import io.ep2p.kademlia.netty.common.NettyConnectionInfo;
+import io.ep2p.kademlia.netty.factory.GsonFactory;
 import io.ep2p.kademlia.node.DHTKademliaNodeAPI;
 import io.ep2p.kademlia.protocol.message.KademliaMessage;
 
@@ -8,15 +10,22 @@ import java.io.Serializable;
 import java.math.BigInteger;
 
 public class NettyKademliaMessageHandler<K extends Serializable, V extends Serializable> extends AbstractKademliaMessageHandler {
-
+    private final GsonFactory gsonFactory;
 
     public NettyKademliaMessageHandler(DHTKademliaNodeAPI<BigInteger, NettyConnectionInfo, K, V> dhtKademliaNodeAPI) {
+        this(dhtKademliaNodeAPI, new GsonFactory.DefaultGsonFactory());
+    }
+
+    public NettyKademliaMessageHandler(DHTKademliaNodeAPI<BigInteger, NettyConnectionInfo, K, V> dhtKademliaNodeAPI, GsonFactory gsonFactory) {
         super(dhtKademliaNodeAPI);
+        this.gsonFactory = gsonFactory;
     }
 
     @Override
     protected KademliaMessage<BigInteger, NettyConnectionInfo, Serializable> toKademliaMessage(String message) {
-
-        return null;
+        return gsonFactory.gson().fromJson(
+                message,
+                new TypeToken<KademliaMessage<BigInteger, NettyConnectionInfo, Serializable>>() {}.getType()
+        );
     }
 }
